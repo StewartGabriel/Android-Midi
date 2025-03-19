0de5a5cb3bed62b368fb6de827843074748c40bc
StewartGabriel <161783846+StewartGabriel@users.noreply.github.com>
2025-03-19 10:57:08 -0700

    Update PlugInInstance.java

--- midi/src/main/java/com/gabriel/midi/PlugInInstance.java
+++ midi/src/main/java/com/gabriel/midi/PlugInInstance.java
@@ -1,23 +1,24 @@
 package com.gabriel.midi;
 import android.app.Activity;
 import android.content.Context;
-import android.media.midi.*; //Not too sure which things I'll need in the future, so I'm including them all for now, also it's cleaner
+import android.media.midi.*; //Not too sure which things I'll need in the future yet, so I'm including them all for now, also it's cleaner
 import android.content.pm.PackageManager;
 import android.util.Log;
 
+import java.io.IOException;
 
 public class PlugInInstance {
-
     private static Activity unityActivity; //THIS NEEDS TO BE KEPT STATIC FOR UNITY TO BE ABLE TO USE IT, IGNORE THE MEMORY LEAK
     private MidiManager midiManager;
-
-    public static void receiveUnityActivity(Activity tActivity) //For the device to know it should only be aware of Unity
-    {
+    private MidiDevice midiDevice;
+//    private Thread listener;
+//    private Thread processor;
+//    private boolean stopThreads = false;
+    public static void receiveUnityActivity(Activity tActivity) { //For the device to know it should only be aware of Unity
         unityActivity = tActivity;
     }
 
-    public void createMidiManager() //Initialize the midiManager, called on startup
-    {
+    public void createMidiManager() {//Initialize the midiManager, called on startup
         Context context = unityActivity.getApplicationContext();
 
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_MIDI)) {
@@ -38,19 +39,17 @@ public class PlugInInstance {
 
     public void openMidiDevice(MidiDeviceInfo deviceInfo) { // Return String for debugging in Unity
         midiManager.openDevice(deviceInfo, midiDevice -> {
-
             if (midiDevice != null) {
+                this.midiDevice = midiDevice;
                 // Set up the MIDI input port
                 MidiDeviceInfo.PortInfo[] ports = midiDevice.getInfo().getPorts();
-                for(MidiDeviceInfo.PortInfo port : ports)
-                {
+                for(MidiDeviceInfo.PortInfo port : ports) {
                     Log.v("Ports", port.toString());
                 }
                 MidiOutputPort outputPort = midiDevice.openOutputPort(0);  // Port index 0 is usually the first output port (in my piano, it is the only port)
                 Log.v("Output Port", "OutputPort: " + outputPort);
 
-                if (outputPort != null) // Set up a listener for incoming MIDI messages
-                {
+                if (outputPort != null) { // Set up a listener for incoming MIDI messages
                     listenToMidiMessages(outputPort);
                 }
             }
@@ -59,18 +58,10 @@ public class PlugInInstance {
 
     private void listenToMidiMessages(MidiOutputPort outputPort) { // Parallel processing to receive and interpret midi messages
         Receiver receiver = new Receiver(); // Custom Receiver class, extends MidiReceiver
-//        ExecutorService listenerPool = Executors.newFixedThreadPool(4);
-//        for (int i = 0; i < 4; i++)
-//        {
         Thread listener = new Thread(() -> outputPort.connect(receiver));
         listener.setPriority(Thread.MAX_PRIORITY);
-
-//            listenerPool.execute(listener);
-//        }
-
         Thread processor = new Thread(() -> {
-            while(true)
-            {
+            while(true) {
                 receiver.processMidiData();
             }
         });
@@ -79,13 +70,10 @@ public class PlugInInstance {
         processor.start();
     }
 
-
-    public int getDeviceAmount() // Check how many devices are connected
-    {
+    public int getDeviceAmount(){ // Check how many devices are connected
         int len = midiManager.getDevices().length;
         Log.v("Devices Connected", "" + len);
-        if(len != 0)
-        {
+        if(len != 0) {
             for(MidiDeviceInfo device : midiManager.getDevices())
                 for (MidiDeviceInfo.PortInfo port: device.getPorts())
                 {
@@ -93,5 +81,25 @@ public class PlugInInstance {
                 }
         }
         return midiManager.getDevices().length;
+    }
+
+    public int disconnectDevices() throws IOException {
+        if(midiDevice != null){
+//            if(listener != null && listener.isAlive()) { //Listener thread shouldn't have anything going, but just in case
+//                stopThreads = true;
+//                listener.interrupt();
+//                Log.v("Thread Closed", "Listener Thread Closed");
+//            }
+//            if(processor != null && processor.isAlive()) { //Interrupt this thread to stop processing incoming data
+//                processor.interrupt();
+//                Log.v("Thread Closed", "Processor Thread Closed");
+//            }
+            midiDevice.close();
+            Log.v("Closed Device", "Midi Device Closed");
+            return 1;
+        } else {
+            Log.v("Closed Device", "No Device to Close");
+            return 0;
+        }
     }
 }
